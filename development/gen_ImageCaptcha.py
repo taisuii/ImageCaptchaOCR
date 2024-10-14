0b5f2679e4d9c184c5814031bf510acc1900f4c4
taisuii <2356955422@qq.com>
2024-10-14 20:46:28 +0800

    添加 .gitignore 文件

--- development/gen_ImageCaptcha.py
+++ development/gen_ImageCaptcha.py
@@ -2,15 +2,15 @@ import os
 import random
 import time
 
-captcha_array = list("0123456789abcdefghijklmnopqrstuvwxyz")
-captcha_size = 4
+captcha_array = list("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+captcha_size = 5
 from captcha.image import ImageCaptcha
 
 if __name__ == '__main__':
     print(captcha_array)
     image = ImageCaptcha()
-    for i in range(10000):
+    for i in range(1):
         image_val = "".join(random.sample(captcha_array, 4))
-        image_name = "./dataset/train/{}_{}.png".format(image_val, int(time.time()))
+        image_name = "./{}_{}.png".format(image_val, int(time.time()))
         print(image_name)
         image.write(image_val, image_name)
